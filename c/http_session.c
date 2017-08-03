4da7aed27a5e7a0ec9ec9c03f965fabcb3babeba
Pablo Slavkin <pslavkin@disenioconingenio.com.ar>
2017-08-03 01:08:36 -0300

    http server callback con case, post y get UP!
    Bueno aho funcan post y get dentro del http_callback genero 2 switch,
    uno para get y otro para post. El tema es que get ncesita devolver algo,
    por eso antes de terminar el callback tiene que llenar la respuesta, en
    cambio post no, solo manda datos, pero no espera nada.. con lo cual post
    se podria mandar un evento a alguna maquina y listo, que siga de ahi, en
    cambio get no porque para cuando la maquina termine y llene la
    respuesta, puede que ya sea tarde...lo hice con un switch incremental
    que supuestamente es una LUT super eficiente.. aunque tengo que llamar 2
    millones de veces a una funcion... parece tener mas sentido hacer una
    arreglo de funciones y llamar una solva vez pasando el index que
    coincide con el numero del GET o POST..voy a probar..

--- c/http_session.c
+++ c/http_session.c
@@ -45,38 +45,55 @@ static void  Welcome_D	(void)	{}
 static void  Welcome_E	(void)	{}
 static void  Welcome_F	(void)	{}
 //----------------------------------------------------------------------------------------------------
+//
+//
+
+void Get_000(unsigned char* Ans, unsigned char* Len) 	{DBG_HTTP_SESSION_PRINT	("G000\r\n");Ans[0]='0';*Len=1;}
+void Get_001(unsigned char* Ans, unsigned char* Len) 	{DBG_HTTP_SESSION_PRINT	("G001\r\n");Ans[0]='1';*Len=1;}
+void Get_002(unsigned char* Ans, unsigned char* Len) 	{DBG_HTTP_SESSION_PRINT	("G002\r\n");Ans[0]='2';*Len=1;}
+void Get_003(unsigned char* Ans, unsigned char* Len) 	{DBG_HTTP_SESSION_PRINT	("G003\r\n");Ans[0]='3';*Len=1;}
+
+void Post_000(void) 					{DBG_HTTP_SESSION_PRINT	("P000\r\n");}
+void Post_001(void) 					{DBG_HTTP_SESSION_PRINT	("P001\r\n");}
+void Post_002(void) 					{DBG_HTTP_SESSION_PRINT	("P002\r\n");}
+void Post_003(void) 					{DBG_HTTP_SESSION_PRINT	("P003\r\n");}
+
+
 void SimpleLinkHttpServerCallback(SlHttpServerEvent_t *pHttpEvent, SlHttpServerResponse_t *pHttpResponse) 
 {
-	DBG_HTTP_SESSION_PRINT	("---->http Event: llego algo!\r\n");
+	unsigned char Token_String[5]="00000"; //del tipo __S_P_123 y me quedo con los ultimos 5 digitos.. en este caso P_123, luego relleno con ceros el P y el _ y puedo convertir a integer para hacer un case hasta 999 tokens de get y otro tanto de post..sufi...
+	unsigned short int Token_Number;
+	unsigned char* Ans_String;
+	unsigned char* Ans_Len;
+	DBG_HTTP_SESSION_PRINT	("---->http Event\r\n");
 	Set_Temp_Led_Effect(Led_THR,0x00FF);
 	switch (pHttpEvent->Event) {
 		case SL_NETAPP_HTTPGETTOKENVALUE_EVENT:
-			DBG_HTTP_SESSION_PRINT	("---->http Event: llego Get %s\r\n",pHttpEvent->EventData.httpTokenName.data);
-			if(!strncmp("__SL_G_XXY",pHttpEvent->EventData.httpTokenName.data,pHttpEvent->EventData.httpTokenName.len)) {
-				DBG_HTTP_SESSION_PRINT	("---->http procesando XXY\r\n");
-                		unsigned char * ptr = pHttpResponse->ResponseData.token_value.data;
-                		ptr[0] = 'O';
-                		ptr[1] = 'K';
-                		pHttpResponse->ResponseData.token_value.len = 2;
-			}
+			strncpy(Token_String+2,(pHttpEvent->EventData.httpTokenName.data)+7,3);
+			Token_Number=Dec_Bcd2Int(Token_String);
+			DBG_HTTP_SESSION_PRINT	("---->http GET number=%d string=%s \r\n",Token_Number,pHttpEvent->EventData.httpTokenName.data);
+			Ans_String=pHttpResponse->ResponseData.token_value.data;
+			Ans_Len=&pHttpResponse->ResponseData.token_value.len;
+			switch (Token_Number)	{
+				case 000: Get_000(Ans_String,Ans_Len); break;
+				case 001: Get_001(Ans_String,Ans_Len); break;
+				case 002: Get_002(Ans_String,Ans_Len); break;
+				case 003: Get_003(Ans_String,Ans_Len); break;
+				}
                 break;
 		case SL_NETAPP_HTTPPOSTTOKENVALUE_EVENT:
-			{
-				unsigned char T[5];
-				unsigned int Token;
-				DBG_HTTP_SESSION_PRINT	("---->http Event: llego post %s\r\n",pHttpEvent->EventData.httpPostData.token_name.data);
-				strncpy(T+2,(pHttpEvent->EventData.httpPostData.token_name.data)+7,3);
-				T[0]=T[1]='0';
-				Token=Dec_Bcd2Int(T);
-				switch (Token)	{
-					case 11:
-						DBG_HTTP_SESSION_PRINT	("---->http procesando 11 %s\r\n",pHttpEvent->EventData.httpPostData.token_value.data);
-						break;
-				}
-			}	
+			strncpy(Token_String+2,(pHttpEvent->EventData.httpPostData.token_name.data)+7,3);
+			Token_Number=Dec_Bcd2Int(Token_String);
+			DBG_HTTP_SESSION_PRINT	("---->http POST number=%d string=%s \r\n",Token_Number,pHttpEvent->EventData.httpPostData.token_value.data);
+			switch (Token_Number)	{
+				case 000: Post_000(); break;
+				case 001: Post_001(); break;
+				case 002: Post_002(); break;
+				case 003: Post_003(); break;
+			}
                 break;
 		default:
-				DBG_HTTP_SESSION_PRINT	("---->http Event: no se que llego...\r\n");
+				DBG_HTTP_SESSION_PRINT	("---->http Event: invalid\r\n");
 		
 	}
  }
