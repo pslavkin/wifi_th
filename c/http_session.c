9bbd42847e759f551e139413ecd97dbbcd998363
Pablo Slavkin <pslavkin@disenioconingenio.com.ar>
2017-08-03 01:27:28 -0300

    get y post con arreglo de punteros a funcion! un lujo!
    Ahora si, se fue la mitad del codigo y duplico velocidad.. hice un
    arreglo de punteros a funcion y las llamo en funcion del numero de get o
    posto.. una estupidez.. simple rapida y facil... y
    ANDA!!
    en realidad no estaba andando bien.. me faltaban los parametros de
    post.. ya lo agregue ahora si funca

--- c/http_session.c
+++ c/http_session.c
@@ -39,61 +39,51 @@ static void  Welcome_Enable_Http	(void)
  Ret=sl_NetAppStart(SL_NET_APP_HTTP_SERVER_ID); 			//prendo
  DBG_HTTP_SESSION_PRINT	("---->http: Habilitado %d \r\n",Ret);
 }
-static void  Welcome_B	(void)	{}
-static void  Welcome_C	(void)	{}
-static void  Welcome_D	(void)	{}
-static void  Welcome_E	(void)	{}
-static void  Welcome_F	(void)	{}
-//----------------------------------------------------------------------------------------------------
-//
-//
+//-------------------Funciones GET---------------------------------------------------------------------------------
+unsigned char Get_Number=0;
+void (*Get[])(unsigned char* Ans,unsigned char *Len);
 
-void Get_000(unsigned char* Ans, unsigned char* Len) 	{DBG_HTTP_SESSION_PRINT	("G000\r\n");Ans[0]='0';*Len=1;}
+void Get_000(unsigned char* Ans, unsigned char* Len) 	
+{
+	DBG_HTTP_SESSION_PRINT	("G000\r\n");
+	Temp2_Fix_Point_Bcd(Ans,0);
+	*Len=7;
+}
 void Get_001(unsigned char* Ans, unsigned char* Len) 	{DBG_HTTP_SESSION_PRINT	("G001\r\n");Ans[0]='1';*Len=1;}
 void Get_002(unsigned char* Ans, unsigned char* Len) 	{DBG_HTTP_SESSION_PRINT	("G002\r\n");Ans[0]='2';*Len=1;}
 void Get_003(unsigned char* Ans, unsigned char* Len) 	{DBG_HTTP_SESSION_PRINT	("G003\r\n");Ans[0]='3';*Len=1;}
 
-void Post_000(void) 					{DBG_HTTP_SESSION_PRINT	("P000\r\n");}
-void Post_001(void) 					{DBG_HTTP_SESSION_PRINT	("P001\r\n");}
-void Post_002(void) 					{DBG_HTTP_SESSION_PRINT	("P002\r\n");}
-void Post_003(void) 					{DBG_HTTP_SESSION_PRINT	("P003\r\n");}
+void (*Get[])(unsigned char* Ans,unsigned char *Len) = {
+Get_000,
+Get_000,
+Get_002,
+Get_003
+};
+//-------------------Funciones POST---------------------------------------------------------------------------------
+void Post_000(unsigned char* Data,unsigned char Len)	{DBG_HTTP_SESSION_PRINT	("P000\r\n");}
+void Post_001(unsigned char* Data,unsigned char Len)	{DBG_HTTP_SESSION_PRINT	("P001\r\n");}
+void Post_002(unsigned char* Data,unsigned char Len)	{DBG_HTTP_SESSION_PRINT	("P002\r\n");}
+void Post_003(unsigned char* Data,unsigned char Len)	{DBG_HTTP_SESSION_PRINT	("P003\r\n");}
 
+void (*Post[])(unsigned char* Data,unsigned char Len) = {
+Post_000,
+Post_001,
+Post_002,
+Post_003
+};
 
+//----------------Callback-----------------------------------------------------------
 void SimpleLinkHttpServerCallback(SlHttpServerEvent_t *pHttpEvent, SlHttpServerResponse_t *pHttpResponse) 
 {
-	unsigned char Token_String[5]="00000"; //del tipo __S_P_123 y me quedo con los ultimos 5 digitos.. en este caso P_123, luego relleno con ceros el P y el _ y puedo convertir a integer para hacer un case hasta 999 tokens de get y otro tanto de post..sufi...
-	unsigned short int Token_Number;
-	unsigned char* Ans_String;
-	unsigned char* Ans_Len;
-	DBG_HTTP_SESSION_PRINT	("---->http Event\r\n");
-	Set_Temp_Led_Effect(Led_THR,0x00FF);
 	switch (pHttpEvent->Event) {
-		case SL_NETAPP_HTTPGETTOKENVALUE_EVENT:
-			strncpy(Token_String+2,(pHttpEvent->EventData.httpTokenName.data)+7,3);
-			Token_Number=Dec_Bcd2Int(Token_String);
-			DBG_HTTP_SESSION_PRINT	("---->http GET number=%d string=%s \r\n",Token_Number,pHttpEvent->EventData.httpTokenName.data);
-			Ans_String=pHttpResponse->ResponseData.token_value.data;
-			Ans_Len=&pHttpResponse->ResponseData.token_value.len;
-			switch (Token_Number)	{
-				case 000: Get_000(Ans_String,Ans_Len); break;
-				case 001: Get_001(Ans_String,Ans_Len); break;
-				case 002: Get_002(Ans_String,Ans_Len); break;
-				case 003: Get_003(Ans_String,Ans_Len); break;
-				}
+		case SL_NETAPP_HTTPGETTOKENVALUE_EVENT:				//se esperan solo gets del tipo __SL_G_000, si llega convierto el 000 a numero y llamo a la funcion numero 000 del arreglo de funciones de un solo tiro!! fua!
+			Get[X_Digits_Dec_Bcd2Int(3,pHttpEvent->EventData.httpTokenName.data+7)](pHttpResponse->ResponseData.token_value.data,&pHttpResponse->ResponseData.token_value.len);
                 break;
-		case SL_NETAPP_HTTPPOSTTOKENVALUE_EVENT:
-			strncpy(Token_String+2,(pHttpEvent->EventData.httpPostData.token_name.data)+7,3);
-			Token_Number=Dec_Bcd2Int(Token_String);
-			DBG_HTTP_SESSION_PRINT	("---->http POST number=%d string=%s \r\n",Token_Number,pHttpEvent->EventData.httpPostData.token_value.data);
-			switch (Token_Number)	{
-				case 000: Post_000(); break;
-				case 001: Post_001(); break;
-				case 002: Post_002(); break;
-				case 003: Post_003(); break;
-			}
+		case SL_NETAPP_HTTPPOSTTOKENVALUE_EVENT:			//se esperan post del tipo __SL_P_000. se convierte a numero y se dispara la funcion dentro del arreglo de funciones... if less code!!
+			Post[X_Digits_Dec_Bcd2Int(3,pHttpEvent->EventData.httpPostData.token_name.data+7)](pHttpEvent->EventData.httpPostData.token_value.data,pHttpEvent->EventData.httpPostData.token_value.len);
                 break;
 		default:
-				DBG_HTTP_SESSION_PRINT	("---->http Event: invalid\r\n");
+			DBG_HTTP_SESSION_PRINT	("---->http Event: invalid\r\n");
 		
 	}
  }
